2bb3fff23d5b3c066c91dbb2883317a9c94e640c
JonWong2020 <jonathannwong50@aol.com>
2022-10-16 21:22:58 -0700

    get routes for users added

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// get all users
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+
+        return res.status(200).json(users);
+    } catch (err) {
+        console.log('Error', err);
+        res.status(500).json(err);
+    }
+};
+
+// get user by id w/
+const getSingleUser = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.params.id })
+            .select('-__v')
+
+        if (!user) {
+            return res.status(404).json({ message: 'No user with that ID '})
+        }
+
+        return res.status(200).json(user);
+    } catch (err) {
+        console.log('Error', err);
+        res.status(500).json(err);
+    }
+};
+// create new user
+
+// update user
+
+// delete user
